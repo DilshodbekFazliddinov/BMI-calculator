27eac4caa9694ecfdfd130fb19a50bbd103ccf5a
DilshodbekFazliddinov <dilshodbek6654@gmail.com>
2022-12-13 22:04:43 +0300

    BMI

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector(".sub");
+const body = document.querySelector("body");
+const formEl = document.querySelector("form");
+const label = document.querySelector(".label");
+const label2 = document.querySelector(".label2");
+const label3 = document.querySelector(".label3");
+const img = document.querySelector(".result-img");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const ism = formEl["ism"].value;
+  const height = formEl["height"].value / 100;
+  const radio = formEl["radio"].value;
+  const ves = formEl["ves"].value;
+  const bmi = ves / height ** 2;
+  label.textContent = ism;
+  label2.textContent = radio;
+
+  if (bmi <= 18.4) {
+    label3.textContent = `${ism} Siz kam vazn siz😟`;
+    label3.style.background = "pink";
+    img.src = "../img/under.jpg";
+  } else if (bmi <= 24.9) {
+    label3.textContent = `${ism} OOO bravo siz alo darajadasiz👍👍👍`;
+    label3.style.background = "green";
+    img.src = "../img/normal.png";
+  } else if (bmi >= 25 && bmi <= 39.9) {
+    label3.textContent = `${ism} Afsus sizda ortiqcha vazn mavjud😜`;
+    label3.style.background = "orange";
+    img.src = "../img/owerweight.jpg";
+  } else if (bmi >= 40) {
+    label3.textContent = `${ism} Siz vapwe kamnavosiz ozishga harakat qiling😊`;
+    label3.style.background = "orange";
+    img.src = "../img/under.jpg";
+  }
+});
